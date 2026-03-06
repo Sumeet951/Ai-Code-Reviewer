147e1e92374d25954a71e09b88b52da6e5a6aba2
Sumeet951 <sg297979@gmail.com>
2026-03-07 01:32:28 +0530

    test-9

--- scripts/review.py
+++ scripts/review.py
@@ -2,8 +2,11 @@ from dotenv import load_dotenv
 import subprocess
 from google import genai
 import os
+# from mistralai import Mistral
 
-load_dotenv()
+# client = Mistral(api_key=os.environ.get("MISTRAL_API_KEY"))
+
+# load_dotenv()
 
 def getDiff():
     diff=subprocess.check_output(["git","show"],text=True)
@@ -11,13 +14,18 @@ def getDiff():
 print(getDiff())
 
 client=genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+
+print("GOOGLE_API_KEY loaded:", os.getenv("GOOGLE_API_KEY") is not None)
 def main():
     diff=getDiff()
     prompt=f"Review the following code changees and provide feedback:\n{diff}"
     response=client.models.generate_content(
-        model="gemini-2.0-flash",
+        model="gemini-3-flash-preview",
         contents=[prompt]
         
     )
     print("Code Review Feedback:")
     print(response.text)
+
+
+main()
