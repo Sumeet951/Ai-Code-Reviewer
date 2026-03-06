859057caf1b45437444b61509f928dd26f7638cd
Sumeet951 <sg297979@gmail.com>
2026-03-07 02:16:22 +0530

    test13

--- scripts/review.py
+++ scripts/review.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 import subprocess
 from google import genai
 import os
+import smtplib
+from email.message import EmailMessage
 # from mistralai import Mistral
 
 # client = Mistral(api_key=os.environ.get("MISTRAL_API_KEY"))
@@ -16,6 +18,17 @@ print(getDiff())
 client=genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 
 print("GOOGLE_API_KEY loaded:", os.getenv("GOOGLE_API_KEY") is not None)
+def send_email(html_content):
+    msg=EmailMessage()
+    msg['Subject']="Code Review Feedback"
+    msg['From']="sg297979@gmail.com"
+    msg['To']="sumeet.ng@somaiya.edu"
+    msg.set_content("Please find the code review feedback below")
+    msg.add_alternative(html_content,subtype='html')
+    with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
+        smtp.login("sg297979@gmail.com",os.getenv("MAIL_APP_PASSWORD"))
+        smtp.send_message(msg)
+    return "Email sent successfully"
 def main():
     diff=getDiff()
     prompt=f"Review the following code changes and provide feedback:\n\n Mandatory:provide the outpu in html that can use to send in mail \n\n{diff}"
@@ -26,6 +39,8 @@ def main():
     )
     print("Code Review Feedback:")
     print(response.text)
+    html=response.text
+    send_email(html)
 
 
 main()
